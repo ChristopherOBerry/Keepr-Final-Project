924a0b76716dbd14e80c3a5a30be81d6abbc909a
Chroberry <chroberry@gmail.com>
2019-08-28 00:08:15 -0600

    Tuesday Night Stopping point

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <h1>Welcome Home {{user.username}}</h1>
-    <button v-if="user.id" @click="logout">logout</button>
-    <router-link v-else :to="{name: 'login'}">Login</router-link>
+    <button class="btn btn-primary" v-if="user.id" @click="logout">logout</button>
+    <router-link class="btn-primary btn" v-else :to="{name: 'login'}">Login</router-link>
   </div>
 </template>
 
